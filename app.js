ae093e4b85447e6f7ad8c9c29f018e49b9e4d535
CooperWindIT <uisupport@cooperwind.in>
2025-06-06 19:06:55 +0530

    Update app.js123

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
 // Start server only after DB connections
 const startServer = async () => {
   try {
-    await cwiDb.createPool();
+    // await cwiDb.createPool();
     await vmsDb.createPool();
 
     app.listen(PORT, host, () => {
