2729365da36c918262788614295143dc7b23366d
CooperWindIT <uisupport@cooperwind.in>
2025-06-06 19:05:11 +0530

    Update config.js11

--- config.js
+++ config.js
@@ -3,23 +3,12 @@ module.exports = {
     
     // Database Configurations for Multiple Databases
     DB_CONFIGS: {
-      CWIdb: {
-        user: 'sa',
-        password: 'sadguru',
-        server:'CWIDEVLAP02/SQLEXPRESS',//'CWIDEVLAP02/SQLEXPRESS',
-        database: 'CWI_db',
-        connectionTimeout: 30000, 
-        requestTimeout: 30000,  
-        options: {
-          encrypt: true, // Azure
-          trustServerCertificate: true, 
-        },
-      },
+      
       CWIVMS: {
-        user: 'sa',
-        password: 'sadguru',
-        server: 'CWIDEVLAP02/SQLEXPRESS',
-        database: 'QAVMS2',
+        user: 'admin',
+        password: 'vms123456',
+        server: 'vms2025.crioumyeym6x.eu-north-1.rds.amazonaws.com',
+        database: 'QAVMS1',
         options: {
           encrypt: true, // Azure
           trustServerCertificate: true, 
