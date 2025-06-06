7acef23d06be4921c83f853083c02e783de1c046
CooperWindIT <uisupport@cooperwind.in>
2025-06-06 22:41:40 +0530

    Update VMSNotifications.js11

--- src/utils/VMSNotifications.js
+++ src/utils/VMSNotifications.js
@@ -9,8 +9,8 @@ class Notify {
             const transporter = nodemailer.createTransport({
                 service: "gmail",
                 auth: {
-                    user: "cwiinfotest@gmail.com",//" info@cooperwindindia.in",//"info@cooperwindindia.in",
-                    pass: "tujg sfnb gewo rmkl" // App Password//"hgxv kbnt vuxp uwaz"//"hgxv kbnt vuxp uwaz", // App Password
+                    user: "info@cooperwindindia.in",//"cwiinfotest@gmail.com ",//"info@cooperwindindia.in",
+                    pass: "hgxv kbnt vuxp uwa" // App Password//"tujg sfnb gewo rmkl z"//"hgxv kbnt vuxp uwaz", // App Password
                 },
             });
 
