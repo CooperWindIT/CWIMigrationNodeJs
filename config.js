dc4d835d26e5c0342cdbb0850c53ba0442906382
CooperWindIT <uisupport@cooperwind.in>
2025-06-11 11:31:50 +0530

    Update config.js1

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ module.exports = {
         password: 'vms123456',
         server: 'vms2025.crioumyeym6x.eu-north-1.rds.amazonaws.com',
         database: 'QAVMS1',
+        connectionTimeout: 30000, 
+        requestTimeout: 30000,
         options: {
           encrypt: true, // Azure
           trustServerCertificate: true, 
