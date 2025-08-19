477e673d62e301b8103674e5bd128d7bf8c751fa
CooperWindIT <uisupport@cooperwind.in>
2025-08-19 17:19:19 +0530

    Update PublicRoutes.js

--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const cwifileUploadApi = require('../controllers/CWIUploadController');
+//const cwifileUploadApi = require('../controllers/CWIUploadController');
 const CWIController = require('../controllers/CWIController');
 
 const router = express.Router();
 
 //#region Organization
 
-router.post('/SignIn', cwifileUploadApi.SignIn);
+//router.post('/SignIn', cwifileUploadApi.SignIn);
 router.get('/SendEmailQueues', CWIController.SendEmailQueues);
 router.get('/SendDailyCheckinSummary', CWIController.SendDailyCheckinsSummary);
 router.get('/PendingCheckOuts', CWIController.PendingCheckOuts);
@@ -15,6 +15,7 @@ router.get('/PendingCheckOuts', CWIController.PendingCheckOuts);
 // router.post('/EditMachines', cwifileUploadApi.EditMachines);
 
 //#end region Organization
+
 
 
 module.exports = router;
